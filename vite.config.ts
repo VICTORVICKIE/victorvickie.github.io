855bcb4abf859c46510389e70402aa622bd2a9e1
VIGNESH KUMAR <45727291+VICTORVICKIE@users.noreply.github.com>
2023-07-05 09:44:30 +0530

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,8 @@ import { defineConfig } from 'vite';
 import wasmPack from 'vite-plugin-wasm-pack';
 
 export default defineConfig({
+    base: '/victorvickie-dev/',
     plugins: [svelte(), wasmPack('./minimax')],
-    build: {
-        outDir: './docs'
-    },
     resolve: {
         alias: {
             '$lib': path.resolve('./src/lib')
