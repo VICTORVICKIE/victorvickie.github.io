4c9c10343803cb7cad3fbf124c80a2aa262b0e42
VIGNESH KUMAR <45727291+VICTORVICKIE@users.noreply.github.com>
2023-07-08 19:50:17 +0530

    wip

--- src/lib/windows/Service.svelte
+++ src/lib/windows/Service.svelte
@@ -1,6 +1,67 @@
-<div class="flex min-h-full w-full">
-    <div class="m-auto flex flex-col">
-        <iconify-icon height="36" width="36" class="m-auto" icon="icon-park:hammer-and-anvil" />
-        <span>Work In Progress</span>
+<div class="m-auto flex flex-wrap gap-8">
+    <div class="card m-auto w-40 rounded-lg border bg-base-300 text-center hover:scale-105 hover:shadow-[0.2rem_0.2rem_0px]">
+        <a target="_blank" href="https://github.com/VICTORVICKIE/sql-notebook" class="card-body">
+            <h2 class="card-title justify-center">
+                <div class="flex flex-col items-center justify-center">
+                    <iconify-icon width="32" height="32" icon="icon-park-outline:database-code" />
+                    <h2>Web Design</h2>
+                </div>
+            </h2>
+        </a>
+    </div>
+    <div class="card m-auto w-40 rounded-lg border bg-base-300 text-center hover:scale-105 hover:shadow-[0.2rem_0.2rem_0px]">
+        <a target="_blank" href="https://jp-jouzu.netlify.app/" class="card-body">
+            <h2 class="card-title justify-center">
+                <div class="flex flex-col items-center justify-center">
+                    <iconify-icon width="32" height="32" icon="icon-park-outline:gate" />
+                    <h2>API Development</h2>
+                </div>
+            </h2>
+        </a>
+    </div>
+    <div class="card m-auto w-40 rounded-lg border bg-base-300 text-center hover:scale-105 hover:shadow-[0.2rem_0.2rem_0px]">
+        <a target="_blank" href="https://monkey-lang-interpreter.netlify.app/" class="card-body">
+            <h2 class="card-title justify-center">
+                <div class="flex flex-col items-center justify-center">
+                    <iconify-icon width="32" height="32" icon="emojione-monotone:monkey-face" />
+                    <h2>Machine Learning</h2>
+                </div>
+            </h2>
+        </a>
+    </div>
+
+    <div class="card m-auto w-40 rounded-lg border bg-base-300 text-center hover:scale-105 hover:shadow-[0.2rem_0.2rem_0px]">
+        <a target="_blank" href="https://github.com/VICTORVICKIE/saravana-enterprise-webapp" class="card-body">
+            <h2 class="card-title justify-center">
+                <div class="flex flex-col items-center justify-center">
+                    <iconify-icon width="32" height="32" icon="icon-park-outline:shopping" />
+                    <h2>Mobile App</h2>
+                </div>
+            </h2>
+        </a>
+    </div>
+    <div class="card m-auto w-40 rounded-lg border bg-base-300 text-center hover:scale-105 hover:shadow-[0.2rem_0.2rem_0px]">
+        <a target="_blank" href="https://github.com/VICTORVICKIE/Aerodynamics" class="card-body">
+            <h2 class="card-title justify-center">
+                <div class="flex flex-col items-center justify-center">
+                    <iconify-icon width="32" height="32" icon="entypo:air" />
+                    <h2>Desktop App</h2>
+                </div>
+            </h2>
+        </a>
+    </div>
+    <div class="card m-auto w-40 rounded-lg border bg-base-300 text-center hover:scale-105 hover:shadow-[0.2rem_0.2rem_0px]">
+        <a target="_blank" href="https://github.com/VICTORVICKIE/Flask-Cryptocurrency" class="card-body">
+            <h2 class="card-title justify-center">
+                <div class="flex flex-col items-center justify-center">
+                    <iconify-icon
+                        width="32"
+                        height="32"
+                        icon="streamline:money-currency-bitcoin-circle-1-crypto-circle-payment-blockchain-finance-bitcoin-currency-money"
+                    />
+                    <h2>Programming related</h2>
+                </div>
+            </h2>
+        </a>
     </div>
 </div>
